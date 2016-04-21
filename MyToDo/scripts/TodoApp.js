640125ffcd79711291ee1ea037b6b0637a3d7815
Godyr112 <kairiss1994@gmail.com>
2016-04-22 01:57:02 +0300

    Second Upload
    Add packages

---
+++ MyToDo/scripts/TodoApp.js
@@ -0,0 +1,13 @@
+﻿var TodoApp = angular.module('TodoApp', []);
+
+TodoApp.controller("mainController", function ($scope, $http) {
+    $http.get('/Home/GetTaskList')
+    .success(function (result) {
+        $scope.TodoList = result;
+
+    })
+    .error(function (data) {
+        console.log(data)
+    })
+
+});
