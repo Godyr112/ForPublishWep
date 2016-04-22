e884e9c7fb4ab86233dd939c6fd82a006b9259cf
Godyr112 <kairiss1994@gmail.com>
2016-04-22 04:20:03 +0300

    Last Update for this night
    Last Update for this night

--- MyTodo/MyTodo/scripts/TodoApp.js
+++ MyTodo/MyTodo/scripts/TodoApp.js
@@ -1,5 +1,6 @@
 ﻿var TodoApp = angular.module('TodoApp', ['ngRoute']);
 
+//Configure angular ng-Route
 TodoApp.config(["$routeProvider", '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/CategoryList", {
@@ -14,21 +15,8 @@ TodoApp.config(["$routeProvider", '$locationProvider', function ($routeProvider,
             redirectTo: "/CategoryList"
         })
     $locationProvider.html5Mode(true);
-
 }]);
-
-
-
-
-
-
-
-
-
-
-
-
-
+//AngularJs main controller
 TodoApp.controller("mainController", function ($scope, $http, $location) {
     
 
@@ -36,29 +24,19 @@ TodoApp.controller("mainController", function ($scope, $http, $location) {
     $http.get('/Home/GetTodoCategorys')
         .success(function (result) {
             $scope.CategoryList = result;
-
         })
         .error(function (data) {
             console.log(data)
     });
-
-    //show to do items
-   
-   
+    //show to do items      
         $http.get('/Home/GetTodoList')
          .success(function (result) {
              
              $scope.TodoList = result;
-
          })
          .error(function (data) {
              console.log(data)
-         });
-
-       
-    
-  
-
+         });           
     //Add new ategory
     $scope.newCategory = "";
     $scope.addCategory = function () {
@@ -76,49 +54,53 @@ TodoApp.controller("mainController", function ($scope, $http, $location) {
     $scope.deleteCategory = function (CategoryList) {
         $http.post("/Home/DeleteCategory", { delCategory: CategoryList })
         .success(function (result) {
-            $scope.CategoryList = result;
-            
+            $scope.CategoryList = result;            
         })
         .error(function (data) {
             console.log(data)
         });
     }
-
-
-
-   // $scope.search();
-  
-
-
+//reset filter
+	/*
+	$scope.reset = function () {
+		$scope.offset = 0;
+		$scope.items = [];
+		$scope.search();
+	};
+//search
+	$scope.search = function () {
+		Todo.query({ q: $scope.query, limit: $scope.limit, offset: $scope.offset },
+			function (items) {
+				var cnt = items.length;
+				$scope.no_more = cnt < 20;
+				$scope.items = $scope.items.concat(items);
+			}
+		);
+	};
+	*/
+   
+   
+//redirect to TaskList template
     $scope.goToTodoList = function (hash) {
-        
-
         $location.path(hash)
     };
-
-
-
-
+//Add new todo TodoText and note
     $scope.newTodoText = "";
     $scope.newTodoNote = "";
     $scope.addTodo = function () {
         $http.post("/Home/AddTodo", { newTodoText: $scope.newTodoText, newTodoNote: $scope.newTodoNote })
         .success(function (result) {
-            $scope.TodoList = result;
-
-           
+            $scope.TodoList = result;           
         })
         .error(function (data) {
             console.log(data)
         });
     }
-
-
+//Delete todo from database
     $scope.deleteTodo = function (TodoList) {
         $http.post("/Home/DeleteTodo", { delTodo: TodoList })
         .success(function (result) {
             $scope.TodoList = result;
-
         })
         .error(function (data) {
             console.log(data)
